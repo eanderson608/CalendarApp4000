f5fc48145e30b7511bbb70d6998f71cc3af3125d
Eric Anderson <eanderson608@gmail.com>
2016-03-04 23:13:56 -0600

    added Event class and Calendar view

---
+++ app/src/main/java/com/cs407/calendarapp4000/Event.java
@@ -0,0 +1,12 @@
+package com.cs407.calendarapp4000;
+
+import java.util.Date;
+
+public class Event {
+    private String id;
+    private String title;
+    private String description;
+    private Date date;
+    private Date time;
+    private String __v;
+}
