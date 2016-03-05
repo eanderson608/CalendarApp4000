33bb58e0f32a3a933bc17a31adf3eaafc1146b8f
Eric Anderson <eanderson608@gmail.com>
2016-03-05 14:06:38 -0600

    set custom adapter in MainActivity, have not tested

--- app/src/main/java/com/cs407/calendarapp4000/MainActivity.java
+++ app/src/main/java/com/cs407/calendarapp4000/MainActivity.java
@@ -1,13 +1,31 @@
 package com.cs407.calendarapp4000;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private ArrayList<Event> eventArrayList;
+    private CustomAdapter adapter;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //initialize fields
+        context = this;
+        listView = (ListView) findViewById(R.id.list_view);
+        eventArrayList = new ArrayList<>();
+        adapter = new CustomAdapter(this, eventArrayList);
+
+        //set adapter
+        listView.setAdapter(adapter);
     }
 }
