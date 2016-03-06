842ad8809262e862d11be29f97494f143e79d840
Eric Anderson <eanderson608@gmail.com>
2016-03-06 00:49:08 -0600

    added toast to display selected date when done button is pressed in AddEventActivity, still having trouble passing params though

--- app/src/main/java/com/cs407/calendarapp4000/AddEventActivity.java
+++ app/src/main/java/com/cs407/calendarapp4000/AddEventActivity.java
@@ -1,10 +1,13 @@
 package com.cs407.calendarapp4000;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class AddEventActivity extends AppCompatActivity {
 
@@ -17,12 +20,29 @@ public class AddEventActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle(R.string.title_addevent);
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_addevent, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+            // add Event
+            case R.id.action_done:
+                String selectedDate = getIntent().getStringExtra("EXTRA_DATE");
+                Toast.makeText(this, selectedDate, Toast.LENGTH_LONG).show();
+
+            default:
+                break;
+        }
+
         return true;
     }
 }
